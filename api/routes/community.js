a572337e31faa5d502157ef6cbf580843d5b461c
jangui <jaimitino@gmail.com>
2021-12-28 23:27:26 +0000

    router for requesting users to join a community

--- api/routes/community.js
+++ api/routes/community.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const mongoose = require('mongoose');
 
 const Community = require('../models/Community.model');
+const User = require('../models/User.model');
 const { uploadImage } = require('../utils');
 
 router.route('/:community').get( async (req, res) => {
@@ -281,6 +282,94 @@ router.route('/join/').post( async (req, res) => {
         return res.status(200).json({
             success: true,
             msg: `Succesfully joined community ${communityName}`,
+        });
+
+    } catch(err) {
+        return res.status(500).json({
+            success: false,
+            msg: `Error: ${err}`
+        });
+    }
+});
+
+router.route('/sendRequest').post( async (req, res) => {
+    const currentUser = res.locals.username;
+    const currentUserID = res.locals.userID;
+    const communityName = req.body.communityName;
+    const requestedUsername = req.body.requestedUsername;
+
+    try {
+        // check user exists
+        const requestedUserDoc = await User.findOne({username: requestedUsername}, 'communityRequests').lean();
+        if (!requestedUserDoc) {
+            return res.status(400).json({
+                success: false,
+                msg: `Error: ${requestedUsername} does not exist.`
+            });
+        }
+
+        // check community exists
+        const community = await Community.findOne({name: communityName}, 'owners members').lean();
+        if (!community) {
+            return res.status(409).json({
+                success: false,
+                msg: `Error: ${communityName} does not exists`
+            });
+        }
+
+        // check we own the community in order to send other user a request
+        const owners = community.owners;
+        let own = false;
+        owners.map(owner => {
+            if (owner.toString() === currentUserID) {
+                own = true; return;
+            }
+        });
+        if (!own) {
+            return res.status(409).json({
+                success: false,
+                msg: `Error: Cannot invite users to a community you don't own.`
+            });
+        }
+
+        // check requested user is not already a member
+        const members = community.members;
+        let isMember = false;
+        members.map(member => {
+            if (member.toString() === requestedUserDoc._id) {
+                isMember = true; return;
+            }
+        });
+        if (isMember) {
+            return res.status(400).json({
+                success: false,
+                msg: `Error: ${requestedUsername} is already a member of ${cummunityName}`,
+            });
+        }
+
+        // check request has not already been sent to user
+        const requests = requestedUserDoc.communityRequests;
+        let sentRequest = false;
+        requests.map( req => {
+            if (req.toString() === community._id.toString()) {
+                sentRequest = true; return;
+            }
+        });
+        if (sentRequest) {
+            return res.status(400).json({
+                success: false,
+                msg: `Error: requests already sent to ${requestedUsername}`,
+            });
+        }
+
+        // send user a request
+        await User.findOneAndUpdate({username: requestedUsername},
+            { $push: { communityRequests: community._id }}
+        );
+
+        return res.status(200).json({
+            success: true,
+            msg: `Succesfully sent ${requestedUsername} a request to join ${communityName}`,
         });
 
     } catch(err) {
